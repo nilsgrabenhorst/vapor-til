1502846cfcdf021fa0b5ddb3cd8df0585d40ac9c
Nils Grabenhorst <nils.grabenhorst@me.com>
2018-06-03 23:32:09 +0200

    first commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,10 +6,12 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
-
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+	
+	router.post("api", "acronyms") { request -> Future<Acronym> in
+		return try request.content
+			.decode(Acronym.self)
+			.flatMap(to: Acronym.self) { acronym in
+				return acronym.save(on: request)
+		}
+	}
 }
